0f36bf0f22f867df213c81a4c1b74c0c3eecc00d
Daniel Eugénio <82546839+dannythe21st@users.noreply.github.com>
2021-09-14 18:01:49 +0100

    test2

---
+++ Test2/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        final String veryNice = "very nice!";
+
+        System.out.println(veryNice);
+        
+    }
+}
