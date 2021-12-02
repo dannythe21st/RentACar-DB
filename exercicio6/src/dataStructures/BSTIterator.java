e2a0982246b9aeece9086227043c09f629634f6a
Daniel Eugénio <82546839+dannythe21st@users.noreply.github.com>
2021-12-02 18:01:42 +0000

    .

---
+++ exercicio6/src/dataStructures/BSTIterator.java
@@ -0,0 +1,25 @@
+package dataStructures;
+
+public class BSTIterator<K,V> implements Iterator{
+
+
+
+    public BSTIterator(){
+
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() throws NoSuchElementException {
+        return null;
+    }
+
+    @Override
+    public void rewind() {
+
+    }
+}
--- exercicio6/src/dataStructures/OrderedDoubleList.java
+++ exercicio6/src/dataStructures/OrderedDoubleList.java
@@ -1,6 +1,7 @@
 package dataStructures;
 
 import dataStructures.Dictionary_Entry.*;
+import dataStructures.DoubleList;
 
 import java.util.Map;
 
@@ -53,8 +54,7 @@ public class OrderedDoubleList<K extends Comparable<K>,V> implements OrderedDict
 
     @Override
     public V insert(K key, V value) {
-        DoubleList.DoubleListNode<Entry<K,V>> node = head;
-        while(!node.getElement().getValue().equals(key) && node != null)
+        /*while(!node.getElement().getKey().equals(key) && node != tail.getNext())
             node = node.getNext();
         if (node == null){
             EntryClass newEntry = new EntryClass(key,value);
@@ -81,33 +81,86 @@ public class OrderedDoubleList<K extends Comparable<K>,V> implements OrderedDict
             DoubleList.DoubleListNode newNode = new DoubleList.DoubleListNode(newEntry, previous, next);
             next.setPrevious(newNode);
             previous.setNext(newNode);
+        }*/
+        /*while(node.getElement().getKey().compareTo(key) < 0)
+            node = node.getNext();
+        if (node.equals(head)){
+            DoubleList.DoubleListNode newNode = new DoubleList.DoubleListNode(e, null, node.getNext());
+            node.getNext().setPrevious(newNode);
+            head = newNode;
         }
-        return node.getElement().getValue();
+        else if (head.)
+        if (node != null && node.getElement().getKey().compareTo(key) == 0){ //existe um elemento com a key
+            Entry e = new EntryClass(key,value);
+            if (node.getElement().getKey().compareTo(head.getElement().getKey()) == 0){
+                DoubleList.DoubleListNode newNode = new DoubleList.DoubleListNode(e, null, node.getNext());
+                node.getNext().setPrevious(newNode);
+                head = newNode;
+            }
+            else if (node.getElement().getKey().compareTo(tail.getElement().getKey()) == 0){
+                DoubleList.DoubleListNode newNode = new DoubleList.DoubleListNode(e, node.getPrevious(), null);
+                node.getNext().setPrevious(newNode);
+                tail = newNode;
+            }
+            else{
+                DoubleList.DoubleListNode newNode = new DoubleList.DoubleListNode(e, node.getPrevious(), node.getNext());
+                node.getPrevious().setNext(newNode);
+                node.getNext().setPrevious(newNode);
+            }
+            return node.getElement().getValue();
+        }
+        else { //inserir elemento novo
+            Entry e = new EntryClass(key,value);
+            DoubleList.DoubleListNode prev = node.getPrevious();
+            DoubleList.DoubleListNode next = node;
+            DoubleList.DoubleListNode newNode = new DoubleList.DoubleListNode<Entry<K,V>>(e, prev, next);
+            node.setPrevious(newNode);
+            return null;
+        }*/
+        DoubleList.DoubleListNode<Entry<K,V>> node = head;
+        Entry e = new EntryClass(key, value);
+        while(node.getElement().getKey().compareTo(key) < 0)
+            node = node.getNext();
+        if (node.getElement().getKey().compareTo(key) == 0){
+            if (node == head){
+                head = new DoubleList.DoubleListNode<>(e, null, head.getNext());
+            }
+            else if (node == tail){
+                tail = new DoubleList.DoubleListNode<>(e, tail.getPrevious(), null);
+            }
+            else{
+                DoubleList.DoubleListNode<Entry<K,V>> prev = node.getPrevious();
+                DoubleList.DoubleListNode<Entry<K,V>> next = node.getNext();
+                DoubleList.DoubleListNode<Entry<K,V>> newNode = new DoubleList.DoubleListNode<>(e, prev, next);
+                prev.setNext(newNode);
+                next.setPrevious(newNode);
+                node = null;
+            }
+            return node.getElement().getValue();
+        }
+        else{
+            DoubleList.DoubleListNode<Entry<K,V>> prev = node.getPrevious();
+            DoubleList.DoubleListNode<Entry<K,V>> newNode = new DoubleList.DoubleListNode<>(e, prev, node);
+            prev.setNext(newNode);
+            node.setPrevious(newNode);
+            return null;
+        }
     }
 
     @Override
     public V remove(K key) {
         DoubleList.DoubleListNode<Entry<K,V>> node = head;
-        while(!node.getElement().getValue().equals(key) && node != null)
+        while(node.getElement().getKey().compareTo(key) < 0)
             node = node.getNext();
-        if (node == null){
+        if (node.getElement().getKey().compareTo(key) > 0)
             return null;
+        else{ //elemento encontrado
+            DoubleList.DoubleListNode<Entry<K,V>> prev = node.getPrevious();
+            DoubleList.DoubleListNode<Entry<K,V>> next = node.getNext();
+            prev.setNext(next);
+            next.setPrevious(prev);
+            return node.getElement().getValue();
         }
-        else if (node.equals(head)){
-            node.getNext().setPrevious(null);
-            head = node.getNext();
-        }
-        else if (node.equals(tail)){
-            node.getPrevious().setNext(null);
-            tail = node.getPrevious();
-        }
-        else{
-            DoubleList.DoubleListNode previous = node.getPrevious();
-            DoubleList.DoubleListNode next = node.getNext();
-            previous.setNext(next);
-            next.setPrevious(previous);
-        }
-        return node.getElement().getValue();
     }
 
     @Override
@@ -123,5 +176,38 @@ public class OrderedDoubleList<K extends Comparable<K>,V> implements OrderedDict
     @Override
     public Entry maxEntry() throws EmptyDictionaryException {
         return tail.getElement();
+    }
+
+
+    private V coise(K key, V value){
+        int i = 0;
+        DoubleList.DoubleListNode<Entry<K,V>> node = head;
+        Entry e = new EntryClass(key, value);
+        while(node.getElement().getKey().compareTo(key) < 0)
+            node = node.getNext();
+        if (node.getElement().getKey().compareTo(key) == 0){
+            if (node == head){
+                head = new DoubleList.DoubleListNode<>(e, null, head.getNext());
+            }
+            else if (node == tail){
+                tail = new DoubleList.DoubleListNode<>(e, tail.getPrevious(), null);
+            }
+            else{
+                DoubleList.DoubleListNode<Entry<K,V>> prev = node.getPrevious();
+                DoubleList.DoubleListNode<Entry<K,V>> next = node.getNext();
+                DoubleList.DoubleListNode<Entry<K,V>> newNode = new DoubleList.DoubleListNode<>(e, prev, next);
+                prev.setNext(newNode);
+                next.setPrevious(newNode);
+            }
+            return node.getElement().getValue();
+        }
+        else{
+
+            DoubleList.DoubleListNode<Entry<K,V>> prev = node.getPrevious();
+            DoubleList.DoubleListNode<Entry<K,V>> newNode = new DoubleList.DoubleListNode<>(e, prev, node);
+            prev.setNext(newNode);
+            node.setPrevious(newNode);
+            return null;
+        }
     }
 }
