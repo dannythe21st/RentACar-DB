f7ab86f84df48a1493d8267b7f2ac80302ee52dc
Daniel Eugénio <82546839+dannythe21st@users.noreply.github.com>
2021-12-05 16:02:42 +0000

    .

--- exercicio6/src/dataStructures/BSTKeyOrderIterator.java
+++ exercicio6/src/dataStructures/BSTKeyOrderIterator.java
@@ -1,38 +1,38 @@
 package dataStructures;
 
-import javax.lang.model.element.Element;
-
 public class BSTKeyOrderIterator<K,V> implements BSTIterator{
 
     private BSTNode<K,V> node;
     private Stack<BSTNode> stack;
+    private BSTNode<K,V> nextToReturn;
     private int currentSize;
 
     public BSTKeyOrderIterator(BSTNode<K,V> root) {
         this.node = root;
         stack = new StackInArray<>();
-        currentSize = 0;
+        this.rewind();
     }
 
     @Override
     public boolean hasNext() {
-        return node.getLeft().isLeaf();
+        return nextToReturn != null;//node.getLeft().isLeaf();
     }
 
     @Override
     public BSTNode<K,V> next() {
         BSTNode<K,V> e = stack.pop();
-        stack.push(node));
+        stack.push(allLeft(node));
+        stack.push(stepRight(allLeft(node)));
         return e;
     }
 
     @Override
     public void rewind() {
-
+        nextToReturn = node;
     }
 
     private BSTNode<K,V> allLeft(BSTNode<K,V> node){
-        BSTNode<K,V> newNode = ;
+        BSTNode<K,V> newNode = node;
         while(newNode.getLeft() != null)
             newNode = newNode.getLeft();
         return newNode;
